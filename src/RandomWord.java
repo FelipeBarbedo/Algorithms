35ca41f645ead393f92df990076be79214eb4aa8
Felipe Barbedo <felipebarbedo54@gmail.com>
2024-07-22 10:40:20 -0300

    Adding problem description.

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -5,9 +5,9 @@ import edu.princeton.cs.algs4.StdIn;
 /*
 Write a program RandomWord.java that reads a sequence of words from standard input
 and prints one of those words uniformly at random. Do not store the words in an array
-or list. Instead, use Knuth’s method: when reading the ith word, select it with probability 1/i
-to be the champion, replacing the previous champion. After reading all the words,
-print the surviving champion.
+or list. Instead, use Knuth’s method: when reading the ith word, select it with
+probability 1/i to be the champion, replacing the previous champion. After reading
+all the words, print the surviving champion.
  */
 public class RandomWord {
     public static void main(String[] args) {
